670c3f2da19468cc31fda95d272fb70a71e8e5f9
beamimc <beatrizcampillo29@gmail.com>
2021-12-20 11:27:17 +0100

    functional gui

--- create_deg_codons_DB.py
+++ create_deg_codons_DB.py
@@ -1,143 +1,149 @@
 #! /usr/bin/env python
-
 """
 @author: beamimc
 
 """
 import pandas as pd
-import math 
-import time
 import os 
 
+from global_variables import *
 from dcg import gcm_multiple
-# global variables
-aas = ['I','M','T','N','K','S','R','L','P','H','Q','V',
-       'A','D','E','G','F','Y','C','W','X']
-codon_aa={
-        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-        'TAC':'Y', 'TAT':'Y', 'TAA':'X', 'TAG':'X',
-        'TGC':'C', 'TGT':'C', 'TGA':'X', 'TGG':'W'
-        }
-deg_nucl={
-         'A':['A'],
-         'T':['T'],
-         'C':['C'],
-         'G':['G'],
-         'B':['G','T','C'],
-         'D':['G','A','T'],
-         'H':['A','T','C'],
-         'K':['G','T'],
-         'M':['A','C'],
-         'N':['A','C','G','T'],
-         'R':['A','G'],
-         'S':['G','C'],
-         'V':['A','C','G'],
-         'W':['A','T'],
-         'Y':['C','T']
-        }
 
-
+def get_all_real_codons(deg_codon):
+    '''
+    given a degenerated codon, get all (non degenerated) codons they work as
+    e.g given AAY, calculte that it works as AAT and AAC
+    if given a non deg codon, it would also work, returning that one
+    e.g given AAT, it would retutn AAT
+    
+    Parameters
+    ----------
+    deg_codon: string
+               codon that we want to know non degenerated codons they work as
+            
+    Returns    
+    -------
+    posibilities: list
+                  list of possible codons            
+    '''
+    posibilities = []
+    for n1 in deg_nucl[deg_codon[0]]:
+        for n2 in deg_nucl[deg_codon[1]]:
+            for n3 in deg_nucl[deg_codon[2]]:
+                posibilities.append(n1+n2+n3)
+    return posibilities
 def deg_codon_to_aa(deg_codon):
-    def get_all_real_codons(deg_codon):
-         posibilities = []
-         for n1 in deg_nucl[deg_codon[0]]:
-             for n2 in deg_nucl[deg_codon[1]]:
-                 for n3 in deg_nucl[deg_codon[2]]:
-                     posibilities.append(n1+n2+n3)
-         # print(posibilities)
-         return posibilities
+    '''
+    given a codon, get the numer of times it codes each aminoacid 
+    
+    Parameters
+    ----------
+    deg_codon : string 
+                codon that we want to know the aminoacids it codes 
+
+    Returns
+    -------
+    dc : dict
+        dict with aas as keys and the numer of times it is coded as value.
+
+    '''
     posibilities = get_all_real_codons(deg_codon)
     df_aas = pd.DataFrame(columns = ['count'], index =aas).fillna(0)  
-    
-    ## remove redundant deg_codons ej TTR codes L 2 times, so we only leave the 
-    ## redundant normal codons TTA, TTG, 
-    ok = True
-    if len(posibilities)>1:
-        coded = []
-        for codon in posibilities:    
-            coded.append(codon_aa[codon])
-        # print(coded)
-        if len(set(coded)) == 1 and deg_codon not in set(codon_aa.keys()):
-            ok = False
-    ## remove redundant deg_codons ej TTA and TTG both code L 1 times
-    ## we only keep 1 option
-    deg_codon not in set(codon_aa.keys())
     
     for codon in posibilities:    
         aa = codon_aa[codon]
         df_aas.loc[aa,'count'] +=1
 
     ##normalize proportions 
+    # e.g AAY codes N 2 times and no other AA, so it would be equivalent to put
+    # it codes it 1 times, since the prob of coding N is 1
+    # e.g AAN codes K 2 times and N 2 times, so it would be equivalent to 
+    # put K 1 time and N 1 time, since the prob of coding each is 0.5 
     prop = (df_aas[df_aas['count']>0]['count'].tolist())
     gcm = gcm_multiple(prop)
     df_aas.loc[:,'count'] = df_aas.loc[:,'count'] / gcm 
-
+    
+    #create dictionaty with result, so it is easier to append to a df
     dc = df_aas['count'].to_dict()
     dc['deg_codon'] = deg_codon
-    return dc, ok 
-
+    return dc
+def get_degenerated_codons():
+    ''' 
+    create all posible combinations (codons) of 3 degenerated nucleotides
+    
+    Returns    
+    -------
+    all_deg_codons: list
+                    list with all posible degenerated codons
+    '''
+    #there are 15 degenerate nucleotides
+    #15x15x15 different posible deg_codons
+    all_deg_codons=[]
+    nucls = list(deg_nucl.keys())
+    for n1 in nucls:
+         for n2 in nucls:
+             for n3 in nucls:
+                 all_deg_codons.append(n1+n2+n3)
+    return all_deg_codons
 
 def create_db_codon():
-    def get_degenerated_codons():
-        ##15x15x15 different posible deg_codons
-        all_deg_codons=[]
-        bases = list(deg_nucl.keys())
-        for n1 in bases:
-             for n2 in bases:
-                 for n3 in bases:
-                     all_deg_codons.append(n1+n2+n3)
-        return all_deg_codons
-    aux = {}
-    for aa in aas:
-        aux[aa]=0
+    '''
+    create 
+    each value represents how many times that aminoacid is coded by that codon
+    e.g df.loc[6,'N'] = 2 would mean that codon 0 (i.e AAH) codes N two times
+    
+    Returns
+    -------
+    df : pandas DataFrame
+        dataframe with all posible degenerated codons
+    df_clean : pandas DataFrame
+        dataframe after removing 'duplicates' of equivalent codons .
+
+    '''
     #get all posible combinations into df
     all_deg_codons = get_degenerated_codons()
     columns = ['deg_codon']
     columns.extend(aas)
     df = pd.DataFrame(columns=columns)
     df_clean = pd.DataFrame(columns=columns)
+    
     #generate df of codon and aa they code proportionally
     for deg_codon in all_deg_codons:
-        dc_aa, ok = deg_codon_to_aa(deg_codon)
+        dc_aa = deg_codon_to_aa(deg_codon)
         df = df.append(dc_aa,ignore_index=True)
-        if ok:
-            df_clean = df_clean.append(dc_aa,ignore_index=True)
- 
-    ### remove codons that do the same 
+        
+    #remove codons that do the same i.e equivalent codons
+    # e.g. AAT, AAC and AAY all code N with probability 1
+    # so only AAT would be kept in the df_clean
+    df_clean = df.copy()
     df_clean = df_clean.drop_duplicates(
           subset = aas,
           keep = 'first').reset_index(drop = True)
     return df, df_clean
 
-if __name__ == '__main__':
+def main():
+    #get current path
     current_path = os.getcwd()
     directory = 'datasets'
     
+    #create directory if it does not exist yet
     if not os.path.exists(directory):
         os.mkdir(directory)
         
-    print('Creating DB and cleaned DB')  
+    #new file names
     filename = 'deg_codons_DB.csv'
     filename_clean = 'deg_codons_DB_clean.csv'
     
+    #execute 
+    print('Creating DB and cleaned DB...') 
     df, df_clean = create_db_codon()
     
+    #save results to csv
     df.to_csv(current_path+'\\'+directory+'\\'+filename,index = False)
     print(f'File saved at {current_path}\\{directory}\\{filename}')
-    
     df_clean.to_csv(current_path+'\\'+directory+'\\'+filename_clean,index = False)
     print(f'File saved at {current_path}\\{directory}\\{filename_clean}')
-
+    return
+    
+if __name__ == '__main__':
+    main()
--- dcg.py
+++ dcg.py
@@ -10,48 +10,9 @@ import time
 import itertools as it
 import random
 import argparse
-# from metaheuristic import SA
-__all__ = [ "generateCodon" ]
 
-# global variables
-aas = ['I','M','T','N','K','S','R','L','P','H','Q','V',
-       'A','D','E','G','F','Y','C','W','X']
-codon_aa={
-        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-        'TAC':'Y', 'TAT':'Y', 'TAA':'X', 'TAG':'X',
-        'TGC':'C', 'TGT':'C', 'TGA':'X', 'TGG':'W'
-        }
-deg_nucl={
-         'A':['A'],
-         'T':['T'],
-         'C':['C'],
-         'G':['G'],
-         'B':['G','T','C'],
-         'D':['G','A','T'],
-         'H':['A','T','C'],
-         'K':['G','T'],
-         'M':['A','C'],
-         'N':['A','C','G','T'],
-         'R':['A','G'],
-         'S':['G','C'],
-         'V':['A','C','G'],
-         'W':['A','T'],
-         'Y':['C','T']
-        }
-        
+from global_variables import *
+
         
 def gcm_multiple(list_numbers):
     if 1 in list_numbers:
@@ -131,10 +92,19 @@ def get_equivalent_codons(df_all, deg_codon):
         codons = df['deg_codon'].tolist()
         if deg_codon in codons:
             return codons
+    
+def get_num_deg_nucl(list_deg_codons):
+    count=0
+    for deg_codon in list_deg_codons:
+        for nucl in deg_codon:
+            if nucl not in 'ATCG':
+                count+=1
+    return(count)
 
 def generateCodon(AAset):
-    #get all posible deg_codons
-    df_all = pd.read_csv('./datasets/deg_codons_DB_clean.csv')
+    #get all posible deg_codons without 'duplicated codons' i.e codons that 
+    #code the same aminoacids with same probabilities
+    df_all = pd.read_csv(CLEAN_CODON_DB)
     
     #filter by restriction 3: all aas coded by same probl
     df_all = restriction_3(df_all)
@@ -147,55 +117,61 @@ def generateCodon(AAset):
     print(len(pull))
 
     ###check if 1 deg_codon codes all AAset
-    #####################################
+    #######################################
     for combi in pull:
-        #check if combination meet restriction 1: 1aa encodeb by just 1 codon
+        #check if combi meets restriction 1: each aa coded by just 1 codon
         rest_1, total_coded_aas = check_restriction_1(df_all,[combi])
         if rest_1:
-            ##if valid combi, check if all aas in AAset are codeb by combi
+            ##if rest_1 is met, check if all aas in AAset are codeb by combi
             valid_combi = True
             for AA in AAset:
                 if AA not in total_coded_aas:
                     # print('not valid')
                     valid_combi = False
             if valid_combi:
-                # if found a valid combi we stop bc it is the smallest
-                # since started checking combination 1 to 1, 2 to 2, etc
+                # if found a valid combi we stop bc it is the smallest (1codon)
                 combi_prop = same_probability(df_all,[combi])
                 return combi_prop
-                # return combi    
-    if len(pull) <= 40 : 
+    greedy = False
+    if len(pull) < 65 : 
         print('brute force')
-        #####solve by brute force --> works but too much time or OutOfMemoryError
-        ##### only do if nº of posible codons is <50 and didnt find only 1 codon
-        ##### best solution guaranteed
+        #solve by brute force -> works but too much time or OutOfMemoryError
+        #only do if nº of posible combinations is < 2000        
+        #directly skip if num of codons in pull is > 65 bc combinations of 2 
+        #out of 65 items is already >2000
+        #best solution guaranteed
         ############################################
         #at most, the max num of deg_codon needed would be the nº of AA in AAset
         max_codons = len(AAset)
         for i in range(2,max_codons+1):
             print('combinations of, ',i)
             combinations = list(it.combinations(pull, i))
-            for combi in combinations:
-                #check if combination meet restriction 1: 1aa encodeb by just 1 codon
-                rest_1, total_coded_aas = check_restriction_1(df_all,combi)
-                if rest_1:
-                    ##if valid combi, check if all aas in AAset are codeb by combi
-                    valid_combi = True
-                    for AA in AAset:
-                        if AA not in total_coded_aas:
-                            # print('not valid')
-                            valid_combi = False
-                    if valid_combi:
-                        # if found a valid combi we stop bc it is the smallest
-                        # since started checking combination 1 to 1, 2 to 2, etc
-                        combi_prop = same_probability(df_all,combi)
-                        return combi_prop
-                        # return combi
+            print(len(combinations))
+            if len(combinations) < 2000:
+                for combi in combinations:
+                    #check if combi meets restrict 1: each aa coded by just 1 codon
+                    rest_1, total_coded_aas = check_restriction_1(df_all,combi)
+                    if rest_1:
+                        ##if rest_1 is met, check if all aas in AAset are coded
+                        valid_combi = True
+                        for AA in AAset:
+                            if AA not in total_coded_aas:
+                                # print('not valid')
+                                valid_combi = False
+                        if valid_combi:
+                            #if found a valid combi, stop bc it is the smallest
+                            #since started checking combinations of 1, then 2,etc
+                            combi_prop = same_probability(df_all,combi)
+                            return combi_prop
+            else:
+                greedy = True
+                break
     else:
-        ######## greedy search--> fast
-        ######## best solution not guaranteed
+        greedy = True
+    if greedy:
+        #variation of greedy search until stop condition is met -> fast
+        #best solution not guaranteed
         #####################################
-        AAset = set(AAset)
         print('greedy')
         stop = False
         best = pull
@@ -203,30 +179,38 @@ def generateCodon(AAset):
         i = 0
         not_better = 0
         while not stop:
-            ##inicialize on random codon, and then greddy search 
+            ##inicialize on random codon, then greddy search to get full combi
             combi = []
-            AAset_ = set(AAset)
+            AAset_ = AAset.copy()
             initial_codon =random.choice(pull)
             combi.append(initial_codon)
-            # print(initial_codon)
+            
+            #get aas coded by the initial codon
             already_coded = set(get_coded_aas(df_all, initial_codon))
-            # print(already_coded)
+
             #new AAset with aas not yet coded
             AAset_ -=already_coded
-            ##make meet restriction 2
+            #meet restriction 2: 
+            #only use deg_codons that do not code aas outside AAset 
             df = restriction_2(df_all,AAset_)
+            
+            #greddy search to finish the combination
+            #stop when there are no more aas in AAset_: all aas coded by combi
             while len(AAset_)>0:
-                # first , get deg_codon that codes the most aas in AAset
+                #first, get deg_codon that codes the most aas in AAset
                 id_max = df[aas].sum(axis=1).idxmax() 
                 max_codon = df.iloc[id_max,0]
                 already_coded = set(get_coded_aas(df, max_codon))
+                #save codon into combi
                 combi.extend([max_codon])
-                # print(max_codon,already_coded)
-                #new AAset with aas not yet coded
+                #new AAset with aas not yet coded                
+                already_coded = set(get_coded_aas(df, max_codon))
                 AAset_ -=already_coded
-                ##make meet restriction 2
+                #meet restriction 2: 
+                #only use deg_codons that do not code aas outside AAset 
                 df = restriction_2(df,AAset_)
-            # print(combi)
+            
+            
             if len(combi)<len(best):
                 best = combi.copy()
                 best_n = len(combi)
@@ -243,14 +227,33 @@ def generateCodon(AAset):
 def main():
     
     good_example = 'GAVCPLIMWF' #### best by brute force is 4 codons {'ATK': 1.0, 'TKT': 1.0, 'KGG': 1.0, 'SYA': 2.0}
+    example = 'GAVCPLIMWFKRED' ## best is 4 codons
+    exampl = 'STYNQKRED'
     
     all_AA = 'SNIRHLGDVCYFKTQPEAMW'
-    charged = 'KRED'
-    charged_ = 'KREDH'
-    polar = 'STYNQ'
-    # #get arguments from cmd    
-    ok = True                           
+    charged = 'KREDH'
+    polar = 'STYNQ'  
+    non_polar = 'GAVCPLIMWF'
+    
+    ok = True       
+    run = True                   
+    
+    #welcome message to user
+    print('\nDEGENERATE CODON DESIGNER')
+    print('Program to desgin the minimun combination of degenerate codons that \
+code all the aminoacids given (AAset) in equal proportions\n')
+    print('Please, provide de AAset you want to code. Aminoacids have to be \
+introduced by their one letter representation')
+    print('E.g: input KRED for coding Lys, Arg, Glu and Asp\n')
+    print('Shortcut inputs:\n \
+    -- all: SNIRHLGDVCYFKTQPEAMW \n \
+    -- charged: KREDH \n \
+    -- polar: STYNQ \n \
+    -- non polar: GAVCPLIMWF')
+    print('E.g: input "all" would be equal to input SNIRHLGDVCYFKTQPEAMW\n')
+    #get input
     AAset = input('Introducde AAset: ').upper()
+    # check input
     if AAset == 'ALL':
         AAset =set(all_AA)
     elif AAset =='CHARGED':
@@ -261,45 +264,42 @@ def main():
         AAset = set(non_polar)
     else:
         ## check if input is correct
-        AAset_ = set(AAset)
-        for aa in AAset_:
+        AAset = set(AAset)
+        for aa in AAset:
             if aa not in aas:
-                return print('wrong AAset')
+                return print('The introduced input was not a valid AAset')
+            
+    #if input is ok run the program
+    # startTime = time.time()
+    #execution
+    combi_prop = generateCodon(AAset)
     
-    if ok:
-        ###execute 
-        startTime = time.time()
-        combi_prop = generateCodon(AAset)
-        print(combi_prop)
-        sorted_combi_prop=sorted(combi_prop.items(), key=lambda kv: kv[1],reverse=True)
-        combi_str = ":".join(str(elem[0]) for elem in sorted_combi_prop)
-        prop_str = ":".join(str(int(elem[1])) for elem in sorted_combi_prop)
-
-        print(f'Output: {combi_str}  ratio {prop_str}\n')
-        executionTime = (time.time() - startTime)
-        # print(executionTime,'\n')
-        
-        ## provide equivalent codons and aas coded by each codon 
-        df_all = pd.read_csv('./datasets/deg_codons_DB.csv')
-        eq_str = ''
-        cd_str = ''
-        for ele in sorted_combi_prop:
-            codon = ele[0]
-            equivalents = get_equivalent_codons(df_all, codon)
-            equivalents.remove(codon)
-            s =", ".join(str(elem) for elem in equivalents)
-            if len(equivalents)>0:
-                # print(f'Instead of {codon} you could equivalently use: {s}\n')
-                eq_str+=(f'Instead of {codon} you could equivalently use: {s}\n')
-            coded_aas = get_coded_aas(df_all, codon)
-            s ="".join(str(elem) for elem in coded_aas)
-            cd_str+=(f'Codon {codon} codes for: {s}\n')
-        print(eq_str)
-        print(cd_str)
+    #sort result in order to provide ratios in descendent order
+    sorted_combi_prop=sorted(combi_prop.items(), key=lambda kv: kv[1],reverse=True)
+    combi_str = ":".join(str(elem[0]) for elem in sorted_combi_prop)
+    prop_str = ":".join(str(int(elem[1])) for elem in sorted_combi_prop)
     
-    else:
-        print('wrong input',AAset)
-        return
+    #show result
+    print(f'Output: {combi_str}  ratio {prop_str}\n')
+    # executionTime = (time.time() - startTime)
+    
+    #provide equivalent codons and aas coded by each codon 
+    df_all = pd.read_csv(CODON_DB)
+    eq_str = ''
+    cd_str = ''
+    for ele in sorted_combi_prop:
+        codon = ele[0]
+        equivalents = get_equivalent_codons(df_all, codon)
+        equivalents.remove(codon)
+        s =", ".join(str(elem) for elem in equivalents)
+        if len(equivalents)>0:
+            eq_str+=(f'Instead of {codon} you could equivalently use: {s}\n')
+        coded_aas = get_coded_aas(df_all, codon)
+        s ="".join(str(elem) for elem in coded_aas)
+        cd_str+=(f'Codon {codon} codes for: {s}\n')
+    print(eq_str)
+    print(cd_str)
+    return
 
 if __name__ == '__main__':
     main()
